9cc7009e5e6b21f050805068f5fcf70d70732c47
Carlos Verdugo <carlosverdugo@proton.me>
2024-05-11 18:06:10 -0700

    Print gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,21 +1,66 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchResponse } from '../interfaces/gifs.interfaces';
+
+const GIPHY_API_KEY ='50vEZwM1h6btza4IQEHTgqjM2xlNmr4K'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  private _tagsHistory: string[] = [];
+  public gifList: Gif[] = [];
 
-  constructor() { }
+  private _tagsHistory: string[] = [];
+  private apiKey:     string = GIPHY_API_KEY;
+  private serviceUrl: string = 'https://api.giphy.com/v1/gifs';
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
   }
 
 
-  searchTag( tag: string ): void {
+  private organizeHistory(tag: string) {
+    tag = tag.toLowerCase();
+
+    if ( this._tagsHistory.includes(tag) ) {
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag );
+    }
+
     this._tagsHistory.unshift( tag );
+    this._tagsHistory = this._tagsHistory.splice(0, 10);
+  }
+
+
+  async searchTag( tag: string ): Promise<void> {
+    if ( tag.length === 0 ) return;
+
+    this.organizeHistory(tag);
+
+    // fetch('https://api.giphy.com/v1/gifs/search?api_key=50vEZwM1h6btza4IQEHTgqjM2xlNmr4K&q=valorant&limit=1')
+    //   .then( res => res.json() )
+    //   .then( data => console.log(data) )
+
+    // const res = await fetch('https://api.giphy.com/v1/gifs/search?api_key=50vEZwM1h6btza4IQEHTgqjM2xlNmr4K&q=valorant&limit=1');
+    // const data = await res.json();
+    // console.log(data);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('limit', 10)
+      .set('q', tag)
+
+    this.http.get<SearchResponse>(`${this.serviceUrl}/search`, { params })
+      .subscribe( (res) => {
+        this.gifList = res.data;
+        console.log({gifs: this.gifList})
+      });
+
   }
 
 }
